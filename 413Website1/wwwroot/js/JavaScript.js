cd1d783d912c0c37c7e24cc4d8e7c1564436e8bd
zackgoodson <zackgoodson@gmail.com>
2023-01-30 23:15:01 -0800

    Add project files.

---
+++ 413Website1/wwwroot/js/JavaScript.js
@@ -0,0 +1,61 @@
+﻿$("#btnSend").click(function () {
+    let grade = (($("#assignment").val() * .50) + ($("#pg").val() * .10) + ($("#quiz").val() * .10) + ($("#midterm").val() * .10) + ($("#final").val() * .10) + ($("#intex").val() * .10));
+
+    //Initialize grade code
+    var letter = "A";
+    //Calculate grade code
+    if (grade >= 94) {
+        letter = "A";
+    }
+    else if (grade >= 90) {
+        letter = "A-";
+    }
+    else if (grade >= 87) {
+        letter = "B+";
+    }
+    else if (grade >= 84) {
+        letter = "B";
+    }
+    else if (grade >= 80) {
+        letter = "B-";
+    }
+    else if (grade >= 77) {
+        letter = "C+";
+    }
+    else if (grade >= 74) {
+        letter = "C";
+    }
+    else if (grade >= 70) {
+        letter = "C-";
+    }
+    else if (grade >= 67) {
+        letter = "D+";
+    }
+    else if (grade >= 64) {
+        letter = "D";
+    }
+    else if (grade >= 60) {
+        letter = "D-";
+    }
+    else {
+        letter = "E";
+    }
+
+    //Display grade code and percentage
+    $('#finalGrade').html("Grade: " + letter + " " + grade + "%");
+
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
