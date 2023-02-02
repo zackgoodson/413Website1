8050d6cf0ccdd09e00e78c99a430d05a557abdca
zackgoodson <zackgoodson@gmail.com>
2023-02-01 17:27:49 -0800

    First Build

---
+++ 413Website1/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace _413Website1.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Calculator()
+        {
+            return View();
+        }
+    }
+}
--- 413Website1/Startup.cs
+++ 413Website1/Startup.cs
@@ -16,14 +16,24 @@ namespace _413Website1
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddControllersWithViews();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            app.UseDefaultFiles();
+
             app.UseStaticFiles();
 
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=HomeController}/{action=Index}/{id?}"
+                );
+            });
         }
     }
 }
